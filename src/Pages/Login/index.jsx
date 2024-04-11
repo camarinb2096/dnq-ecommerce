0ed778ac24757ed689da3980d67d91451b4712eb
camilo_marin <camarinb20@gmail.com>
2024-04-10 19:19:18 -0500

    fix login image border

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -8,7 +8,7 @@ function LoginForm() {
       <Layout>
         <div className='flex w-full min-h-screen'>
           <div className='w-1/2 h-screen flex flex-col justify-center px-8 bg-gray-200'>
-            <img src='https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='login' className='w-full h-full object-cover' />
+            <img src='https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='login' className='w-full h-full object-cover rounded-2xl mt-24 mb-8' />
           </div>
           <form className='w-1/2 h-screen flex flex-col gap-8 justify-center pl-8 rounded-s bg-gray-200'>
             <p className='font-bold text-5xl'>
