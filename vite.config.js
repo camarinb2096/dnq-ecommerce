58e82fda070436dae1a7c5b1fc5d721823e7660b
camilo_marin <camarinb20@gmail.com>
2024-05-20 14:55:39 -0500

    change output dir

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',  // Asegúrate de que esto está bajo el objeto 'build'
+    outDir: 'dist',  // Asegúrate de que esto está bajo el objeto 'build'
   }
 });
